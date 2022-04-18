55f6ebbee10e271d8c1e358d17cdccd7b0995212
dengjinan <dengjinan@bytedance.com>
2022-04-18 16:33:27 +0800

    fluter工程增加发布测试

---
+++ ios/Runner/myViewController.h
@@ -0,0 +1,16 @@
+//
+//  myViewController.h
+//  Runner
+//
+//  Created by bytedance on 2022/4/18.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface myViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
